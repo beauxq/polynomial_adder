aa8e9d781f5c5dad761f980e3dfe395f0466b0c0
beauxq <beauxq@user.noreply.github.com>
2015-09-23 17:21:40 -0500

    notes on what is in Term and Polynomial classes

--- polynomial_adder/Polynomial.h
+++ polynomial_adder/Polynomial.h
@@ -1,4 +1,10 @@
 #ifndef POLYNOMIAL_H_INCLUDED
 #define POLYNOMIAL_H_INCLUDED
 
+/*	notes:
+	+
+	add term (this will insert if the term with matching exponent isn't already there,
+		and add to the term if it's already there,
+		then it will delete the term if the coefficient is zero) */
+
 #endif  // POLYNOMIAL_H_INCLUDED
