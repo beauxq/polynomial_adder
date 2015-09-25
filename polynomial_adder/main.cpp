2f3460b37922c6d7f94adfeab630ffe36c4dc7df
Natalie Bradshaw <eilatanbz@gmail.com>
2015-09-25 16:03:39 -0500

    asdf
    asdf

--- polynomial_adder/main.cpp
+++ polynomial_adder/main.cpp
@@ -1,16 +1,17 @@
 #include "Term.h"
 #include "Polynomial.h"
-
+#include <iostream>
+#include <string>
 
 int main()
 {
-	string variable = "I can't figure out how to run this";
-	if (variable =! "Test")
+	std::string variable = "I can't figure out how to run this";
+	if (variable != "Test")
 	{
-		std::cout >> " Natalie is unsuccessfull in testing." >> endl;
+		std::cout << " Natalie is unsuccessfull in testing." << std::endl;
 	}
 	else
-		std::cout >> "Testing is currently working" >> endl;
+		std::cout << "Testing is currently working" << std::endl;
 
 	return 0;
 }
